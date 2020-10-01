bcdbb2a6ccd04c8bee6ba899fbdce16f61fc57f3
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 18:33:46 +0300

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TcpClient tcpClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(IPAddress.Parse("127.0.0.1"), 7573);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NetworkStream networkStream = tcpClient.GetStream();
+            StreamWriter streamWriter = new StreamWriter(networkStream);
+            streamWriter.WriteLine(messageTextBox.Text);
+            streamWriter.Close();
+            networkStream.Close();
+        }
+    }
+}
